ae42bff0c377241bbcc5c5f75ebdad7f55e4de9c
AidanClarkGomez <161875254+AidanClarkGomez@users.noreply.github.com>
2024-05-16 12:44:04 -0300

    part 1

---
+++ GeoLocationAPI/script.js
@@ -0,0 +1,20 @@
+navigator.geolocation.getCurrentPosition(
+    function(position) {
+        const latitude = position.coords.latitude
+        const longitude = position.coords.longitude
+        const coords = [latitude, longitude]
+        console.log(latitude, longitude)
+
+        var map = L.map('map').setView(coords, 13)
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
+
+        L.marker(coords).addTo(map).bindPopup('A pretty CSS popup.<br> Easily customizable.').openPopup();
+
+        console.log(`https://www.google.com/maps/@${latitude,longitude},15z`)
+    },
+    function(){
+        alert("Could not get position.")
+    }
+)
+
